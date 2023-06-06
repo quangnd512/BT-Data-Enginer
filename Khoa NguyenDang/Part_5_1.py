e91aa1ea82d0048735fd0fa47a3f5225a8209e90
BI11-122 <101243446+KhoaNguyen1804@users.noreply.github.com>
2023-06-06 11:18:49 +0700

    khoa

---
+++ Khoa NguyenDang/Part_5_1.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+import jsonlines
+import html2text
+
+url = 'https://tuyensinhso.vn/diem-chuan/diem-chuan-cac-truong-dai-hoc-hoc-vien-khu-vuc-mien-bac-c47979.html'
+
+def get_diem_chuan_mien_bac_link(url):
+    diemchuanmienbacLink = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    links = soup.find_all('a')
+    keywords = ["diem-chuan"]
+    excluded_keywords = ["ban-tin",
+                         "tin-moi"]
+    for link in links:
+        href = link.get('href')
+        if href and any(keyword in href for keyword in keywords) and not any(ex_keyword in href for ex_keyword in excluded_keywords):
+            diemchuanmienbacLink.append(href)
+    return diemchuanmienbacLink
+
+result5_1 = get_diem_chuan_mien_bac_link(url)
+print("Điểm chuẩn khu vực miền Bắc: ", result5_1)
+print("--------------------------------------------------------")
+
+output_file = 'data_5_1.jsonl'
+with jsonlines.open(output_file, mode='w') as writer:
+    for link in result5_1:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.content, "html.parser")
+        title_element = soup.find("h1", class_="detail_title")
+        content_element = soup.find("div", class_="detail-content")
+        if title_element and content_element:
+            title = title_element.text
+            content = content_element.encode_contents().decode()  # Get HTML content as string
+            converter = html2text.HTML2Text()
+            converter.body_width = 0  # Preserve line breaks
+            markdown_content = converter.handle(content)
+            writer.write({'title': title,
+                            'content': markdown_content,
+                            'url': link})
+
+# Load the JSON lines file and convert content to readable markdown format
+markdown_data = []
+with jsonlines.open(output_file) as file:
+    for line in file.iter():
+        title = line['title']
+        content = line['content']
+        url = line['url']
+        markdown = f"## {title}\n\n{content}\n\n[Source]({url})\n"
+        markdown_data.append(markdown)
+
+# Join all markdown data into a single string
+markdown_output = "\n".join(markdown_data)
+
+# Save the markdown output to a new file
+output_markdown_file = 'output_5_1.md'
+with open(output_markdown_file, 'w', encoding='utf-8') as file:
+    file.write(markdown_output)
+
+print("Data saved to", output_markdown_file)
+print("-----------------------------------------")
+
+ 
